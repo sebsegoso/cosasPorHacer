92707fbe1029f0c46e1c602a75c20f96e15cddfc
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-08-16 03:18:45 -0400

    add styles

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 const categorias = (() => {
     const contenido = document.getElementById("Contenido")
-    const bgColor = (color) => document.body.style.backgroundColor = color
+    const aparecer = (() => contenido.style.opacity = "1")
+    const bgColor = (color) => {
+        let estiloBody = document.body.style
+        estiloBody.backgroundColor = color
+        estiloBody.color = "#003049"
+    }
 
     
     return {
         musica: (color) => {
-            let texto = '<a href="https://www.youtube.com/channel/UCa3DVlGH2_QhvwuWlPa6MDQ" target="_blank">Jaime Altozano</a> analizando el disco "El mal querer" de Rosalía, una obra de arte'
-            let url = '<br /><iframe width="560" height="315" src="https://www.youtube.com/embed/NgHXFTgaVT0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
+            let texto = 'Jaime Altozano analizando el disco "El mal querer" de Rosalía, una obra de arte. Si no lo conoces el bro te explica por qué es una obra de arte'
+            let url = '<br /><iframe width="100%" height="315" src="https://www.youtube.com/embed/NgHXFTgaVT0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
             return contenido.innerHTML = texto + url,
-            bgColor(color)
+            bgColor(color),
+            aparecer()
         },
         cine: (color) => {
-            let texto = 'Un clásico de Quentin Tarantino'
-            let url = '<br /><iframe width="560" height="315" src="https://www.youtube.com/embed/s7EdQ4FqbhY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
+            let texto = 'Para ver: Un clásico de Quentin Tarantino'
+            let url = '<br /><iframe width="100%" height="315" src="https://www.youtube.com/embed/s7EdQ4FqbhY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
             return contenido.innerHTML = texto + url,
-            bgColor(color)
+            bgColor(color),
+            aparecer()
         },
         comida: (color) => {
             let texto = 'Esto era más fácil de hacer de lo que creía'
-            let url = '<br /><iframe width="560" height="315" src="https://www.youtube.com/embed/xurmz6BAUw8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
+            let url = '<br /><iframe width="100%" height="315" src="https://www.youtube.com/embed/xurmz6BAUw8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />'
             return contenido.innerHTML = texto + url,
-            bgColor(color)
+            bgColor(color),
+            aparecer()
         }
     }
 })()
@@ -30,11 +38,11 @@ const botonCine = document.getElementById("Cine")
 const botonComida = document.getElementById("Comida")
 
 botonMusica.addEventListener("click", () => {
-    categorias.musica()
+    categorias.musica('#ef476f')
 })
 botonCine.addEventListener("click", () => {
-    categorias.cine()
+    categorias.cine("#f77f00")
 })
 botonComida.addEventListener("click", () => {
-    categorias.comida()
+    categorias.comida('#fcbf49')
 })
